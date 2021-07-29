2a9635d34f0ce4ddce857e398c5b3d9397ded672
tobie <tobiewee@gmail.com>
2021-07-29 21:26:52 +0800

    Updated Sealed Class Examples

--- src/main/kotlin/com/company/kotlin/sealed/SealedExample.kt
+++ src/main/kotlin/com/company/kotlin/sealed/SealedExample.kt
@@ -1,27 +1,30 @@
 package com.company.kotlin.sealed
 
+// Creating this here is OK, as it is in the same base package. Try creating similar class or object in another package!
+class Parallelogram(val angleA: Int, val angleB: Int) : Shape(4, angleA + angleB)
+
 fun main() {
-    val newShape: Shape = Rectangle
-    println(
-        when (newShape) {
-            Rectangle -> "I am a rectangle, I have ${newShape.side} sides, with each angle being ${newShape.angle}"
-            Triangle -> "I am a triangle, I have ${newShape.side} sides, with each angle being ${newShape.angle}"
-            Square -> "I am a square, I have ${newShape.side} sides, with each angle being ${newShape.angle}"
-        }
-    )
+    println("--------------- Java Sample ---------------")
+    val sample = Sample()
+    sample.callingFunction()
+    sample.testFunction(ShapeJava.Rectangle)
 
-    println()
-
-    //Java version
-//    val sample = Sample()
-//    sample.callingFunction()
-//    sample.testFunction(ShapeJava.Rectangle)
-
-    println()
-
-    //Kotlin version
+    println("--------------- Kotlin Solution ---------------")
     val solution = Solution()
     solution.callerFunction()
     solution.testFunction(Rectangle)
     solution.testFunction(Triangle)
+    solution.testFunction(Parallelogram(60, 120))
+
+    println("--------------- Kotlin Local Example---------------")
+    val newShape: Shape = Parallelogram(50, 130)
+    println(
+        when (newShape) {
+            is Parallelogram -> "I am a parallelogram, I have ${newShape.side} sides, " +
+                    "with angles of ${newShape.angleA} and ${newShape.angleB} on each side which adds to ${newShape.angle}"
+            Rectangle -> "I am a rectangle, I have ${newShape.side} sides, with each angle being ${newShape.angle}"
+            Square -> "I am a square, I have ${newShape.side} sides, with each angle being ${newShape.angle}"
+            Triangle -> "I am a triangle, I have ${newShape.side} sides, with each angle being ${newShape.angle}"
+        }
+    )
 }
--- src/main/kotlin/com/company/kotlin/sealed/Shape.kt
+++ src/main/kotlin/com/company/kotlin/sealed/Shape.kt
@@ -9,10 +9,10 @@ object Rectangle : Shape(4, 90)
 object Square : Shape(4, 90)
 object Triangle : Shape(3, 60)
 
-
 class Solution {
     fun callerFunction() {
         testFunction(Square)
+        //testFunction(Shape(1, 40)) //sealed classes cannot be instantiated
     }
 
     fun testFunction(shape: Shape) {
@@ -21,6 +21,8 @@ class Solution {
                 Rectangle -> "I am a rectangle, I have ${shape.side} sides, with each angle being ${shape.angle}"
                 Triangle -> "I am a triangle, I have ${shape.side} sides, with each angle being ${shape.angle}"
                 Square -> "I am a square, I have ${shape.side} sides, with each angle being ${shape.angle}"
+                is Parallelogram -> "I am a parallelogram, I have ${shape.side} sides, my angles are ${shape.angleA} and ${shape.angleB}." +
+                        "The total is angle is ${shape.angle}."
             }
         )
     }
